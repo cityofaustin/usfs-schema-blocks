4ff50ff422339a480225102b69b5456bb9a53547
easherma <ericandrewsherman@gmail.com>
2019-02-05 18:44:52 -0600

    add race placeholder for reals this time

--- src/blocks/raceBlocks.js
+++ src/blocks/raceBlocks.js
@@ -1,51 +1,51 @@
 const raceBlocks = {
   schema: {
     race: {
-      type: "string",
+      type: 'string',
       enum: [
-        "white",
-        "latino",
-        "black",
-        "asian",
-        "indian",
-        "arab",
-        "native",
-        "hawaiian",
-        "preferNot",
-        "other"
+        'white',
+        'latino',
+        'black',
+        'asian',
+        'indian',
+        'arab',
+        'native',
+        'hawaiian',
+        'preferNot',
+        'other'
       ],
       enumNames: [
-        "White or Euro-American",
-        "Latino or Hispanic American",
-        "Black, Afro-Caribbean, or African American",
-        "East Asian or Asian American",
-        "South Asian or Indian American",
-        "Middle Eastern or Arab American",
-        "Native American or Alaskan Native",
-        "Native Hawaiian or Other Pacific Islander",
-        "Prefer not to say",
-        "Other"
+        'White or Euro-American',
+        'Latino or Hispanic American',
+        'Black, Afro-Caribbean, or African American',
+        'East Asian or Asian American',
+        'South Asian or Indian American',
+        'Middle Eastern or Arab American',
+        'Native American or Alaskan Native',
+        'Native Hawaiian or Other Pacific Islander',
+        'Prefer not to say',
+        'Other'
       ]
     },
-    otherRace: { type: "string", "ui:collapsed": true }
+    otherRace: { type: 'string', 'ui:collapsed': true }
   },
   ui: {
     race: {
-      "ui:title": "Race",
-      "ui:options": {
+      'ui:placeholder': '- Select race -',
+      'ui:title': 'Race',
+      'ui:options': {
         hideOnReviewIfFalse: true
       }
     },
     otherRace: {
-      "ui:placeholder": "- Select race -",
-      "ui:title": " ",
-      "ui:options": {
-        expandUnder: "race",
-        expandUnderCondition: "other",
+      'ui:title': ' ',
+      'ui:options': {
+        expandUnder: 'race',
+        expandUnderCondition: 'other',
         hideOnReviewIfFalse: true
       }
     }
   }
-};
+}
 
-export default raceBlocks;
+export default raceBlocks
